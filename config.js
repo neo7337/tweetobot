c906131fdaf4d5125057b5300ea231c76d5f9cf1
Aditya <adityak007mr@gmail.com>
2019-03-15 01:28:00 +0530

    configuration

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    consumer_key: ENV['KEY_C'],
-    consumer_secret: ENV['KEY_S'],
-    access_token: ENV['AT'],
-    access_token_secret: ENV['ATS']
+    consumer_key: process.env.KEY_C,
+    consumer_secret: process.env.KEY_S,
+    access_token: process.env.AT,
+    access_token_secret: process.env.ATS
 }
